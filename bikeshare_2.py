4a8cb55bf21cd21916eb7793895c4dcc45461cc9
Patrick <p.haussler@gmx.de>
2023-07-31 08:27:38 +0200

    updated function get.filters(), line 8 in the python file

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -7,7 +7,7 @@ CITY_DATA = {'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Asks user to specify a city, month, and day to analyze the input.
 
     Returns:
         (str) city - name of the city to analyze
